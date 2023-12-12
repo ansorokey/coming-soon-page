05a48e000b234148f2dad73e2f25a9a6668520f0
ansorokey <ansorokey@gmail.com>
2023-12-12 15:09:46 -0500

    readme update and comments added

--- README.md
+++ README.md
@@ -4,7 +4,14 @@ A coming soon page for a website
 This project was made by following [How To Make A Website Coming Soon Page Using HTML CSS & JavaScript with Time Date Countdown](https://youtu.be/_LExTzOhe7s?si=QzpScGeyrDD_vNT1)
 
 ## Key Features and Technologies
+- JavaScript, HTML, CSS
+- DOM Manipulation
+- CSS Animations
+- JavaScript Date Objects
+- Asynchronous
 
 ## How To Use
+- The remaining time to a set date/time will countdown on the main page.
 
 ## Future Features
+- None currently planned
--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// Reference HTML Elements
 const [dd, hh, mm, ss] = [document.querySelector('#dd'), document.querySelector('#hh'), document.querySelector('#mm'), document.querySelector('#ss')]
 
+// Pick any date in the future
+// Use .getTime to return the date as a number in milliseconds
 const countDownDate = new Date('Oct, 16, 2025 00:00:00').getTime();
+
+// Create an interval to run every second
 const x = setInterval(() => {
+    // get the current time (in milliseconds)
     const now = new Date().getTime();
+
     const diff = countDownDate - now;
 
     // Provided time conversions
@@ -16,6 +23,8 @@ const x = setInterval(() => {
     mm.innerHTML = minutes;
     ss.innerHTML = seconds;
 
+    // if the date has passed (negative difference)
+    // Set times to 00s
     if (diff < 0) {
         clearInterval(x);
         dd.innerHTML = '00';
